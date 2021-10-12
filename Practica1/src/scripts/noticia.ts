a0cbbc07fd7c7f6ecaaeec0146631dfb83062ae8
Erick Gamboa Valle <is710115@iteso.mx>
2021-10-11 19:54:46 -0500

    Creating Tequila app

---
+++ Practica1/src/scripts/noticia.ts
@@ -0,0 +1,10 @@
+declare let axios;
+
+export class Noticia {
+    getAll(): Promise<any> {
+        const busqueda = document.getElementById("site-search").value;
+        console.log(busqueda);
+        const url: string = "https://newsapi.org/v2/everything?q="+busqueda+"&sortBy=popularity&apiKey=45cc4febdcfd4fe59738f0cc9beaecb0";
+        return axios.get(url);
+    }
+}
